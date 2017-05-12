701d9ad8d23d4738e25fa2787395516efc682ed4
Max Hunt <max@huntw3.com>
2017-05-12 22:45:58 +0100

    Document, refactor a little.

--- src/od117_walker/core.clj
+++ src/od117_walker/core.clj
@@ -1,10 +1,11 @@
 (ns od117-walker.core
   (:gen-class)
-  (:require [clj-http.client :as client]
-            [pl.danieljanus.tagsoup :as ts]
-            [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clj-http.client :as client]
+            [pl.danieljanus.tagsoup :as ts]))
 
 (defn find-and-parse-page
+  "Download the given wiki page and run it through the parser."
   [page]
   (->> page
        (str "http://od117.wikidot.com/")
@@ -12,9 +13,8 @@
        (:body)
        (ts/parse-string)))
 
-(defonce aardvark-html (find-and-parse-page "aardvark"))
-
 (defn find-id
+  "Find the html element with a given id in the given html."
   [html id]
   (if (= (:id (second html)) id)
     html
@@ -25,6 +25,7 @@
           (recur (rest childs)))
         nil))))
 
+;; Set of authors to ignore, since we don't want them on the final graph.
 (def authors #{"/catalogatrix-maridia"
                "/isabella-yutani"
                "/john-9eb99c7f-e482-492f-8807-dc6f208b5c60"
@@ -34,58 +35,85 @@
                "/research-droid-8608c29d-d2e6-441e-9d6c-edaf932a7e82"})
 
 (defn extract-links
+  "Extract the links from the given html. Ignores authors."
   [html]
-  (reduce (fn [ls h] (apply hash-set (concat ls
-                                             (reduce (fn [ls h2] (if-not (string? h2)
-                                                                   (if (and (= (first h2) :a)
-                                                                            (not (authors (:href (second h2)))))
-                                                                     (conj ls (:href (second h2)))
-                                                                     ls)
-                                                                   ls)) #{} (ts/children h)))))
-          #{} (ts/children html)))
+  (reduce (fn [ls h] (apply hash-set
+                            (concat ls
+                                    (reduce (fn [ls h2] (if-not (string? h2)
+                                                          (if (and (= (first h2) :a)
+                                                                   (not (authors (:href (second h2)))))
+                                                            (conj ls (:href (second h2)))
+                                                            ls)
+                                                          ls))
+                                            #{}
+                                            (ts/children h)))))
+          #{}
+          (ts/children html)))
 
 (defn extract-author
+  "Extract the author of the entry from the html. Assumes there is only one author linked."
   [html]
-  (reduce (fn [ls h] (apply hash-set (concat ls
-                                             (reduce (fn [ls h2] (if-not (string? h2)
-                                                                   (if (and (= (first h2) :a)
-                                                                            (authors (:href (second h2))))
-                                                                     (conj ls (:href (second h2)))
-                                                                     ls)
-                                                                   ls)) #{} (ts/children h)))))
-          #{} (ts/children html)))
+  (first (reduce (fn [ls h] (apply hash-set
+                                   (concat ls
+                                           (reduce (fn [ls h2] (if-not (string? h2)
+                                                                 (if (and (= (first h2) :a)
+                                                                          (authors (:href (second h2))))
+                                                                   (conj ls (:href (second h2)))
+                                                                   ls)
+                                                                 ls))
+                                                   []
+                                                   (ts/children h)))))
+                 []
+                 (ts/children html))))
 
 (defn walk
+  "Walk through the wiki building a graph representation. Takes a list of starting pages and a set of pages to ignore."
   [start-points exclusions]
-  (loop [to-check start-points visited exclusions out {}]
+  (loop [to-check start-points
+         visited exclusions
+         out {}]
     (if-let [next (first to-check)]
       (if (visited next)
-        (recur (rest to-check) visited out)
+        (recur (rest to-check)
+               visited
+               out)
         (do
           (println next)
-          (if-let [next-para (try (find-id (find-and-parse-page next) "page-content") (catch Exception e nil))]
-            (recur (concat (rest to-check) (extract-links next-para)) (conj visited next) (assoc out next {:links (extract-links next-para)
-                                                                                                           :author (extract-author next-para)}))
-            (recur (rest to-check) (conj visited next) out))))
+          (if-let [next-para (try
+                               (find-id (find-and-parse-page next) "page-content")
+                               (catch Exception e nil))]
+            (recur (concat (rest to-check) (extract-links next-para))
+                   (conj visited next)
+                   (assoc out next {:links (extract-links next-para)
+                                    :author (extract-author next-para)}))
+            (recur (rest to-check)
+                   (conj visited next)
+                   out))))
       out)))
 
-(def author-map {"/catalogatrix-maridia" "aquamarine"
-                 "/isabella-yutani" "brown1"
-                 "/john-9eb99c7f-e482-492f-8807-dc6f208b5c60" "burlywood1"
-                 "/major-kaspar-parmenides" "cadetblue1"
-                 "/prof-cuthburt-robinson-smithe" "chocolate1"
-                 "/qitik-von-gratzk" "darkorchid2"
-                 "/research-droid-8608c29d-d2e6-441e-9d6c-edaf932a7e82" "darkslategray1"})
+(def author-colours {"/catalogatrix-maridia" "aquamarine"
+                     "/isabella-yutani" "brown1"
+                     "/john-9eb99c7f-e482-492f-8807-dc6f208b5c60" "burlywood1"
+                     "/major-kaspar-parmenides" "cadetblue1"
+                     "/prof-cuthburt-robinson-smithe" "chocolate1"
+                     "/qitik-von-gratzk" "darkorchid2"
+                     "/research-droid-8608c29d-d2e6-441e-9d6c-edaf932a7e82" "darkslategray1"})
 
 (defn graphify-one
+  "Create graphviz dot language entries for given graph node (in internal map format)."
   [[key {:keys [links author]}]]
-  (apply str (apply str (map #(str \" key \" " -> " \" % \" ";\n") links)) \" key \" " " "[color=" (get author-map (first author)) ",style=filled];\n"))
+  (apply str
+         (apply str (map #(str \" key \" " -> " \" % \" ";\n") links))
+         \" key \" " " "[color=" (get author-colours author) ",style=filled];\n"))
 
 (defn graphify
+  "Convert a map representation of the graph into a string of graphviz dot language."
   [g]
   (let [vals (map graphify-one (seq g))]
     (apply str "digraph G {" (apply str vals) "}")))
 
+;; Start points that will ensure we hit the whole graph.
+;; Manually entered, a little bit of a hack.
 (def start-points ["/caseinogenic-blood-rose"
                    "/anachronistic-shrubs"
                    "/depths-of-anguish"
@@ -94,7 +122,7 @@
                    "/mendacious-islands"])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Calculate and print the graph of OD-117 to foo.dot."
   [& args]
   (with-open [wrtr (io/writer "foo.dot")]
     (.write wrtr (graphify (walk start-points (conj authors "/redacted"))))))
