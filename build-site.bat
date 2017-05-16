b8d9c4be3f1f5ef1efb7368d713315b41b421e08
Max Hunt <max@huntw3.com>
2017-05-16 19:40:41 +0100

    Make batch script consistent.

--- build-site.bat
+++ build-site.bat
@@ -5,4 +5,4 @@ if not exist "output" mkdir output
 call lein run
 
 dot -Tpng .\output\ranked.dot -o"output/ranked.png"
-dot.exe -Tpng .\output\general.dot -o"output/general.png"
+dot -Tpng .\output\general.dot -o"output/general.png"
